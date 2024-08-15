78e136cccf10fbb7031639abe3388a4bef76ade3
信念是我的砥柱 <1173326004@QQ.com>
2024-08-15 13:16:25 +0800

    地址更新

--- js/api/http.js
+++ js/api/http.js
@@ -1,10 +1,2 @@
-// var http1="http://192.168.0.200:11001/"
-// var http1="http://192.168.0.50:11002/"
-var http2="http://notinboy.tpddns.cn:10030/"
-
-var ws1 = "ws://192.168.0.103:8082"
-var http1 = "http://117.50.117.118:11001/"
-// var http1 ="http://192.168.2.51:10031/"
-
-   // var http1 ="http://192.168.2.92:10031/"
-//var http1="http://notinboy.tpddns.cn:11001/"
+// var http1 = "http://117.50.117.118:11001/"
+var http1 ="http://192.168.2.230:10001/"
